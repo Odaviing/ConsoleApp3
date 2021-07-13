792e6e11382cc67fc3d4d214753b9b3d3a9c9217
Odaviing <tes5alduin@gmail.com>
2021-07-13 15:13:21 +0300

    Update Program.cs

--- ConsoleApp3/Program.cs
+++ ConsoleApp3/Program.cs
@@ -4,104 +4,150 @@ namespace ConsoleApp3
 {
     class Program
     {
+        public static int size_check()
+        {
+            string value;
+            int size = 0;
+            bool check = false;
+            while (!check)
+            {
+                value = Console.ReadLine();
+                check = int.TryParse(value, out size);
+                if (check == false)
+                {
+                    Console.WriteLine("YOU NEED TO INPUT THE NUMBER!");
+                }
+            }
+            return size;
+        }
+
+        public static double array_element()
+        {
+            string element;
+            double d_element = 0;
+            bool el_check = false;
+            while (!el_check)
+            {
+                element = Console.ReadLine();
+                el_check = double.TryParse(element, out d_element);
+                if (el_check == false)
+                {
+                    Console.WriteLine("I SAID REAL NUMBERS!");
+                }
+            }
+            return d_element;
+        }
+
+        public static void sorting(double[] some_array, int some_size, int choice, int sort)
+        {
+            double temp;
+            if (sort == 1)
+            {
+                for (int i = 0; i < some_size; i++)
+                {
+
+                    for (int j = 0; j < some_size - i - 1; j++)
+                    {
+                        if (some_array[j] > some_array[j + 1])
+                        {
+                            temp = some_array[j];
+                            some_array[j] = some_array[j + 1];
+                            some_array[j + 1] = temp;
+
+                        }
+                    }
+
+                }
+            }
+
+            if (sort == 2)
+            {
+                for (int i = 0; i < some_size; i++)
+                {
+
+                    for (int j = 0; j < some_size - i - 1; j++)
+                    {
+                        if (some_array[j] < some_array[j + 1])
+                        {
+                            temp = some_array[j];
+                            some_array[j] = some_array[j + 1];
+                            some_array[j + 1] = temp;
+
+                        }
+                    }
+
+                }
+            }
+            Console.WriteLine("That's your sorted array, witness it in all of it's glory:");
+            for (int i = 0; i < some_size; i++)
+            {
+                Console.WriteLine(some_array[i]);
+            }
+
+            Console.WriteLine("And that's the same array after your choice:");
+                if (choice == 1)
+            {
+                for (int i = 0; i < some_size; i++)
+                {
+                    if (some_array[i] % 2 == 0)
+                    {
+                        Console.WriteLine(some_array[i]);
+                    }
+                }
+            }
+            if (choice == 2)
+            {
+                for (int i = 0; i < some_size; i++)
+                {
+                    if (i % 2 == 0)
+                    {
+                        Console.WriteLine(some_array[i]);
+                    }
+                }
+            }
+
+
+        }
+
+        public static int choosing()
+        {
+            int final_choice = 0;
+            while (final_choice != 1 && final_choice != 2)
+            {
+                final_choice = size_check();
+                if (final_choice != 1 && final_choice != 2)
+                {
+                    Console.WriteLine("CHOOSE 1 OR 2 YOU IDIOT!");
+                }
+            }
+            return final_choice;
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Your cock is big, but how big is your array?");
-            int size = Convert.ToInt32(Console.ReadLine());
-            double[] arr = new double[size];
-            Console.WriteLine("Now lets fill this big array, shall we?");
-            string number = "";
-            double i_check;
+            Console.WriteLine("Show me the size of the array");
+            int arr_size = size_check();
+            double[] arr = new double[arr_size];
+            Console.WriteLine("Now let's fill this array");
+            for (int i = 0; i < arr_size; i++)
+            {
+                Console.WriteLine("Show me some numbers");
+                arr[i] = array_element();
+                Console.WriteLine("Your element is:" + arr[i]);
+                Console.WriteLine("");
+            }
             
-            for (int i = 0; i < size; i++)
-            {
-                bool check = false;
-                Console.WriteLine("Enter the number:");
-                while (!check)
-                {
-                    number = Console.ReadLine();
-                    check = Double.TryParse(number, out i_check);
-                    if (check == false)
-                    {
-                        Console.WriteLine("ARE YOU RETARDED OR SOMETHING? YOU SHOULD ENTER THE NUMBERS!");
-                    }
-                }
-                arr[i] = Convert.ToDouble(number);
-                Console.WriteLine(arr[i]);
-            }
-            Console.WriteLine("Now lets do some sorting of our array");
-            double temp;
-            for (int i = 0; i < size; i++)
-            {
-                
-                for (int j = 0; j < size - i - 1; j++)
-                {
-                    if (arr[j] < arr[j + 1])
-                    {
-                        temp = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp;
-                    }
-                    
-                }
-            }
             Console.WriteLine("Lets check this out");
-            for (int i = 0; i < size; i++)
-            {
-                if (arr[i] % 2 == 0)
-                {
-                    Console.WriteLine(arr[i]);
-                }
-            }
+            Console.WriteLine("Press 1 if you want to see a even elements, press 2 if you want to see elements with even indexes");
+            int output_choice = choosing();
+            Console.WriteLine("It's time to sort our array. Press 1 if you want to sort elements in ascending order, or press 2 for descending order");
+            int sort_choice = choosing();
+            sorting(arr, arr_size, output_choice, sort_choice);
 
 
-            Console.WriteLine("Now lets do this one more time, enter the size:");
-            int size1 = Convert.ToInt32(Console.ReadLine());
-            double[] arr1 = new double[size];
-            Console.WriteLine("Fill the new one:");
-            string number1 = "";
-            double i_check1;
 
-            for (int i = 0; i < size; i++)
-            {
-                bool check1 = false;
-                Console.WriteLine("Enter the number:");
-                while (!check1)
-                {
-                    number1 = Console.ReadLine();
-                    check1 = Double.TryParse(number1, out i_check1);
-                    if (check1 == false)
-                    {
-                        Console.WriteLine("ARE YOU RETARDED OR SOMETHING? YOU SHOULD ENTER THE NUMBERS!");
-                    }
-                }
-                arr[i] = Convert.ToDouble(number1);
-                Console.WriteLine(arr[i]);
-            }
-            Console.WriteLine("Lets sort this one too");
-            double temp1;
-            for (int i = 0; i < size; i++)
-            {
-
-                for (int j = 0; j < size - i - 1; j++)
-                {
-                    if (arr[j] < arr[j + 1])
-                    {
-                        temp1 = arr[j];
-                        arr[j] = arr[j + 1];
-                        arr[j + 1] = temp1;
-                    }
-
-                }
-            }
-            Console.WriteLine("Lets check this out");
-            for (int i = 0; i < size; i++)
-            {
-                if (i % 2 == 0)
-                {
-                    Console.WriteLine(arr[i]);
-                }
-            }
+            
         }
     }
 }
